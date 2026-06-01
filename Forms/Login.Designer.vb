f55bdff1bbff4caecec68174380194b220959f8b
Pruax <47928063+Pruax@users.noreply.github.com>
2026-06-01 12:00:22 -0500

    Add files via upload

---
+++ Forms/Login.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.lblUsername = New System.Windows.Forms.Label()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(133, 41)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(100, 20)
+        Me.txtUsername.TabIndex = 0
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(133, 83)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(100, 20)
+        Me.txtPassword.TabIndex = 1
+        '
+        'lblUsername
+        '
+        Me.lblUsername.AutoSize = True
+        Me.lblUsername.Location = New System.Drawing.Point(55, 41)
+        Me.lblUsername.Name = "lblUsername"
+        Me.lblUsername.Size = New System.Drawing.Size(55, 13)
+        Me.lblUsername.TabIndex = 2
+        Me.lblUsername.Text = "Username"
+        '
+        'lblPassword
+        '
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Location = New System.Drawing.Point(55, 86)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(53, 13)
+        Me.lblPassword.TabIndex = 3
+        Me.lblPassword.Text = "Password"
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(58, 143)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogin.TabIndex = 4
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(174, 143)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 5
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(330, 210)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.lblUsername)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Name = "frmLogin"
+        Me.Text = "Login "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents lblUsername As Label
+    Friend WithEvents lblPassword As Label
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents btnCancel As Button
+End Class
--- Forms/Login.vb
+++ Forms/Login.vb
@@ -1,1 +1,91 @@
+﻿
+Imports System.Data
+Imports System.Data.SqlClient
 
+Public Class frmLogin
+
+    Public userLogin As String
+    Public loginAttempt As Integer = 0
+    Public existingUsers() As String = {"Bkassin scooter3", "Djeter kstate21", "Ljames hooper31", "Lwayne shawty1"}
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        'Need to check the user database for credintals. 
+        'If credintals match allow user to login in to correct home screen view
+        'If incorrect give error message and number of attempts. If user exceeds 3 attempts lock user out
+
+        ConnectToSQL()
+
+    End Sub
+
+    'Connect to SQL method
+
+    Private Sub ConnectToSQL()
+
+        Dim con As New SqlConnection
+        Dim cmd As New SqlCommand
+        Dim Password As String
+        Dim Password2 As String
+        Dim userName As String
+
+
+
+        Try
+            If
+                'change the data source and initial catalog according to your sql server engine and data base
+                con.ConnectionString = "Data Source = Azure; Initial Catalog = YOUR-DB; Integrated Security = True"
+                con.Open()
+
+                cmd.Connection = con
+                'change the data fields names and table according to your database
+                cmd.CommandText = " SELECT  UserName, Password FROM   Table-Name WHERE   (UserName = '" & txtUsername.Text & "' ) AND (Password = '" & txtPassword.Text & "')"
+
+                Dim lrd As SqlDataReader = cmd.ExecuteReader()
+                If lrd.HasRows Then
+                    While lrd.Read()
+
+                        'Do something here
+                        Password = lrd("Password").ToString()
+                        userName = lrd("UserName").ToString()
+
+                        Password2 = txtPassword.Text()
+
+                        If Password = Password2 And userName = txtUsername.Text Then
+
+                            MessageBox.Show("Logged in successfully as " & userName, "", MessageBoxButtons.OK, MessageBoxIcon.Information
+                                        )
+                            frmMain.Show()
+                            Me.Hide()
+
+                            'Clear all fields
+                            txtPassword.Text = ""
+                            txtUsername.Text = ""
+
+                        End If
+
+                    End While
+
+                Else
+                    MessageBox.Show("Username and Password do not match..", "Authentication Failure", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                    'Clear all fields
+                    txtPassword.Text = ""
+                    txtUsername.Text = ""
+                End If
+
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Error while connecting to SQL Server." & ex.Message)
+
+        Finally
+            con.Close() 'Whether there is error or not. Close the connection.
+
+        End Try
+
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        'btnCancel will cancel the login operations and close the system.
+        Me.Close()
+    End Sub
+
+End Class
